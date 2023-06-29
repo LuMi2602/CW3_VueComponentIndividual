4f931662ef3ba06f95284ad7df44bbf956ae1f3d
LuMi2602 <wedimulie@gmail.com>
2023-06-29 16:20:18 +0400

    update

--- my-app/src/components/CheckoutComponent.vue
+++ my-app/src/components/CheckoutComponent.vue
@@ -1,18 +1,27 @@
 <template>
     <div>
       <h2>Checkout</h2>
-      <h3>count</h3>
-      
+      <h3>Cart</h3>
       {{cartCount}}
-      <ul>
-        <li v-for="lesson in cart" :key="lesson._id">
+
+      <p>
+          <strong>First Name:</strong>
+          <input type="text" v-model="inputl" >
+        </p>
+
+        <p>
+          <strong>Phone Number:</strong>
+          <input type="text" v-model="Inputn">
+        </p>
+      
+      <div id="item" v-for="lesson in cart" :key="lesson._id">
           <img id="img" :src="lesson.images" />
         <p>{{ lesson.Subject}}</p>
         <p>Location:{{ lesson.Location}}</p>
         <p>Price:{{ lesson.Price}}</p>
           <button @click="removeFromCart(lesson)">Remove</button>
-        </li>
-      </ul>
+        
+     </div>
     </div>
   </template>
   
@@ -29,11 +38,72 @@
       required: false,
     },
     },
-    methods: {
-      removeFromCart(lesson) {
-        this.$emit('removeLesson', lesson);
-      },
-    },
+    data(){
+    return {
+      inputl: '',
+      Inputn: '',
+    }
+
+  },
+  
   };
   </script>
   
+  <style>
+    body {
+      color: white;
+      font-family: Arial, Helvetica, sans-serif
+    }
+
+    footer {
+
+      color: white;
+    }
+
+    #app {
+      /* background-image: url(public/assets/scl.png); */
+      background-size: contain;
+      padding-left: 1.5rem;
+
+    }
+
+    #item {
+
+      text-decoration-color: white;
+      background-color: #212142;
+      max-width: 320px;
+      margin-right: 3rem;
+      margin-top: 1rem;
+
+      margin-bottom: 3rem;
+      padding: 1rem;
+      border: 1px solid #212142;
+      border-radius: 1rem;
+      display: inline-block;
+
+    }
+    
+
+    img {
+      box-sizing: border-box;
+      padding-left: 0%;
+      object-fit: cover;
+      width: 100%;
+      text-decoration-color: aliceblue;
+
+    }
+
+    #searchinput {
+      width: 40%;
+      padding-left: 35%;
+      margin-top: 1rem;
+
+
+    }
+
+    #submit {
+      font-size: medium;
+      position: relative;
+      margin-bottom: 1rem;
+    }
+  </style>
